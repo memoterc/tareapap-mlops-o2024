53d85e4166c668f274f23a33e1eade016cdb95dd
memo <areviromem@outlook.com>
2024-10-05 00:53:03 -0600

    updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from scipy.stats import chi
 from fastapi import FastAPI
 import pandas as pd
 import numpy as np
@@ -15,4 +16,21 @@ def health():
         "status" : "Todo Gucci"
     }
 
+@app.post("/predict")
+async def predict(data : dict):
+    
+    data = pd.DataFrame([data.dict()])
+    prediction = model.predict(data)
 
+    data["Y"] = prediction 
+    data.to_csv(path = "./data/new_data.csv", index = False)
+
+    return {
+        "prediciton" : int(prediction)
+    }
+
+
+@app.get("/tests")
+async def test_dist():
+
+    train_data = pd.read_csv(r"./data/credit_train.csv")
